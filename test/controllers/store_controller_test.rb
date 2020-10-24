a2a05b175c71cf94e070a9e84213eaf55e5c9418
ivanprats <hi@ivanprats.dev>
2020-10-25 00:07:15 +0200

    StoreController#index tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,11 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+
+    assert_select 'nav a', minimum: 4
+    assert_select 'main [data-semantic="product-card"]', Product.count
+    assert_select 'h2', products(:ruby).title
+    assert_select 'main [data-semantic="price"]', /\$[,\d]+\.\d\d/
   end
 
 end
