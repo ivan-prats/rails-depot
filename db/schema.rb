fc3cd1a737eec455501a3e5f6bc1123750f9b27f
ivanprats <hi@ivanprats.dev>
2020-10-25 00:36:39 +0200

    D1: Generate Cart scaffold + concern/current_cart.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_090048) do
+ActiveRecord::Schema.define(version: 2020_10_24_223123) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
