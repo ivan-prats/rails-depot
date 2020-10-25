b120a660b42c4e24d03149483cd00afd0017ffc3
ivanprats <hi@ivanprats.dev>
2020-10-25 12:49:05 +0100

    D3: "Add to Cart" button
    The most important things I did / learnt:
    
    - "Add to Cart" button in Store page. Using the  <%= button_to 'Add to Cart', line_items_path(product_id: product) %> helper
    
    - In the line_items_controller: we included a module, and used its private function like it was inside the Class
    - We call the set_cart method before the create action, through the before_action hook
    
    - Instead of starting a new LineItem model object through the "new" method, we use the "@cart.line_items.build(product: product)" method. To add through the connection with the @cart. I did comment a second option that I already knew how to do
    
    - When creating a LineItem: we redirect to the Cart page, not to the line_items_controller#show. And we add styles and tags to show the current line_items in the cart
    
    - Tests for all these

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,7 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
+
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -24,11 +27,16 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+
+    # Option 1 : starting the LineItem through its @cart connection
+    @line_item = @cart.line_items.build(product: product)
+    # Option 2: starting LineItem from scratch
+    # @line_item = LineItem.new(cart: @cart, product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
