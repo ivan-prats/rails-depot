86155a617b149b8be2edc754e1e4babc7da2f7b8
ivanprats <hi@ivanprats.dev>
2020-10-28 01:14:56 +0100

    F2 AJAX Babyyyyy (when adding line_item)
    Pretty interesting! The server executes back some javascript function instead of turning back html or json

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -32,8 +32,10 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url, notice: 'Line item was successfully added.', flash: { notice_type: 'success' } }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
+        format.js
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
