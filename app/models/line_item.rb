cdb6ce720ea9b1b43e4e3764c194b39bf758573c
ivanprats <hi@ivanprats.dev>
2020-10-31 14:49:56 +0100

    Testing a bit better the LineItem.total_price logic. To make sure that it always saves into the DB the correct value

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,14 +2,9 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
 
-  before_save :set_total_price
+  before_save { self.total_price = total_price }
+
   def total_price
     quantity * product.price
-  end
-
-  private
-
-  def set_total_price
-    self.total_price = total_price
   end
 end
--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should always show the correct total_price' do
+    cart = Cart.create
+    line_item = LineItem.new({ product: products(:ruby), cart: cart })
+
+    assert_operator line_item.total_price, :==, products(:ruby).price * line_item.quantity
+
+    line_item.quantity += 1
+    assert_operator line_item.total_price, :==, products(:ruby).price * line_item.quantity
+    line_item.quantity += 2
+    assert_operator line_item.total_price, :==, products(:ruby).price * line_item.quantity
+    line_item.quantity = 0
+    assert_operator line_item.total_price, :==, products(:ruby).price * line_item.quantity
+  end
+
+  test 'should always save into DB the correct total_price' do
+    cart = Cart.create
+    quantity = 123_456
+    line_item = LineItem.create({ product: products(:ruby), cart: cart, quantity: quantity })
+    assert_operator line_item.total_price, :==, products(:ruby).price * quantity
+
+    line_item_by_quantity = LineItem.find_by total_price: products(:ruby).price * quantity
+    assert(line_item_by_quantity.id == line_item.id)
+
+    updated_quantity = quantity + 10
+    line_item.quantity = updated_quantity
+    assert line_item.save
+    line_item_by_quantity = LineItem.find_by total_price: products(:ruby).price * updated_quantity
+    assert(line_item_by_quantity.id == line_item.id)
+  end
 end
