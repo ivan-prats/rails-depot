6c1eabfbfbd72cba48d033853a6ad3c949b57684
ivanprats <hi@ivanprats.dev>
2020-11-01 16:17:57 +0100

    Testing everything we just did

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -26,12 +26,52 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should create order' do
-    assert_difference('Order.count') do
+  test 'cart buttons should be hidden in new order (checkout page)' do
+    post line_items_url, params: { product_id: products(:ruby).id }
+    get new_order_url
+    assert_response :success
+
+    # I would prefer to assert the CSS property "display" of the buttons, but dunno how to do it
+    assert_select '#cart .button_to .hide-on-orders', 4
+    assert_select '#cart .button_to', 4
+  end
+
+  test 'should NOT create order if there is nothing in the cart' do
+    assert_no_difference('Order.count') do
+      post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
+    end
+    assert_redirected_to store_index_url
+  end
+
+  test 'should create order if there is at least one item in the cart' do
+    post line_items_url, params: { product_id: products(:ruby).id }
+    assert_difference('Order.count', 1) do
       post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
     end
 
+    order = Order.all.last
+    assert_equal order.line_items.size, 1
+    assert_equal order.line_items.first.product_id, products(:ruby).id
+
     assert_redirected_to store_index_url
+    assert_equal flash[:notice], 'Thank you for your order!'
+    assert_equal flash[:notice_type], 'success'
+  end
+
+  test 'should delete current cart from DB and from session after creating an order' do
+    post line_items_url, params: { product_id: products(:ruby).id }
+
+    current_cart_id = session[:cart_id]
+    assert_not_nil current_cart_id
+
+    assert_difference('Order.count', 1) do
+      post orders_url, params: { order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type } }
+    end
+
+    assert_nil(session[:cart_id])
+    assert_raises(ActiveRecord::RecordNotFound) do
+      Cart.find(current_cart_id)
+    end
   end
 
   test 'should show order' do
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'add_line_items_from_cart should add the line_items to the order and take them out of the cart' do
+    # We build a Cart in session
+    cart = Cart.new
+    cart.add_product(products(:ruby))
+    cart.add_product(products(:two))
+
+    order = Order.new
+    order.add_line_items_from_cart(cart)
+
+    assert_equal order.line_items.size, 2
+    assert_equal order.line_items[0].product_id, products(:ruby).id
+    assert_equal order.line_items[1].product_id, products(:two).id
+    order.line_items.each do |item|
+      assert_nil item.cart_id
+    end
+  end
 end
