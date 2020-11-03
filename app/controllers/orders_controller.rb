1284e980aa508c9449628c82358185f530bc52b3
ivanprats <hi@ivanprats.dev>
2020-11-03 23:55:20 +0100

    I1: configuring and setting up emails!

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,6 +41,8 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
 
+        OrderMailer.received(@order).deliver_later
+
         format.html { redirect_to store_index_url, notice: 'Thank you for your order!', flash: { notice_type: 'success' } }
         format.json { render :show, status: :created, location: @order }
       else
